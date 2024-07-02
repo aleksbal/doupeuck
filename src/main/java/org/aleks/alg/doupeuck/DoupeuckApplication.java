921ab8e6f1b71050cb1693f76f7c808d75d6b0d7
aleksbal <aleksandar.balaban@gmail.com>
2024-07-02 18:03:54 +0200

    First commit

---
+++ src/main/java/org/aleks/alg/doupeuck/DoupeuckApplication.java
@@ -0,0 +1,13 @@
+package org.aleks.alg.doupeuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoupeuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoupeuckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/aleks/alg/doupeuck/DoupeuckApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aleks.alg.doupeuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoupeuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
